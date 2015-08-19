e724acbfcbe305a405cc26e9a528787ffaa69e8b
Gabor Nagy Farkas <nfgabor@gmail.com>
2015-08-19 21:44:05 +0200

    Initial commit
    Created the Xcode project, removed the
    .xib files.

---
+++ NFStepperExample/NFStepperExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFStepperExample
+//
+//  Created by Gabor Nagy Farkas on 19/08/15.
+//  Copyright (c) 2015 Gabor Nagy Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
